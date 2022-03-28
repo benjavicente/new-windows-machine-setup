3917fee4f7f90bcda32c1d91d31618fddf269953
Benjamín Vicente <62021328+benjavicente@users.noreply.github.com>
2022-03-28 11:45:21 -0300

    Update utils.sh

--- bash-config/utils.sh
+++ bash-config/utils.sh
@@ -29,3 +29,21 @@ fix_wsl2_interop() {
         fi
     done
 }
+
+
+## SSH
+### Remember keys
+# alias ssh-setup='eval $(ssh-agent) &> /dev/null && ssh-add'
+function ssh-setup {
+    RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+    if [ -z "$SSH_AUTH_SOCK" ]; then
+        # Check for a currently running instance of the agent
+        RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+        if [ "$RUNNING_AGENT" = "0" ]; then
+            # Launch a new instance of the agent
+            ssh-agent -s &> $HOME/.ssh/ssh-agent
+        fi
+        eval `cat $HOME/.ssh/ssh-agent`
+    fi
+    ssh-add
+}
